390ae7e7d7bbb1333899e1e356dbe2c33d829317
Xu <xujijun@126.com>
2014-12-26 15:21:48 +0800

    null value convertion and permission checking

--- src/com/xjj/anes/mvc/interceptor/ControllerInterceptor.java
+++ src/com/xjj/anes/mvc/interceptor/ControllerInterceptor.java
@@ -1,6 +1,7 @@
 package com.xjj.anes.mvc.interceptor;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +20,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xjj.anes.annotation.SessionChecking;
 import com.xjj.anes.bean.common.LoginUser;
 import com.xjj.anes.bean.common.ResultBean;
+import com.xjj.anes.cache.CacheConstants;
+import com.xjj.anes.cache.CacheManager;
 import com.xjj.anes.constants.MessageCode;
 import com.xjj.anes.service.common.CacheSessionService;
 import com.xjj.anes.service.common.SessionService;
@@ -28,6 +32,7 @@ public class ControllerInterceptor implements HandlerInterceptor {
 	private Set<String> notCheckURISet = new HashSet<String>();
 	private final String REQTIME = "_REQTIME_";
 	private final String LOGINUSER = "_LOGINUSER_";
+	private static Map<String, String> uriPermissionIdMap = null;
 
 	//private static SessionService sessionService = CoreApplicationContext.getApplicationContext().getBean(SessionService.class);
 	//private static CacheSessionService cacheSessionService = CoreApplicationContext.getApplicationContext().getBean(CacheSessionService.class);
@@ -41,6 +46,7 @@ public class ControllerInterceptor implements HandlerInterceptor {
 		this.notCheckURISet = notCheckURISet;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		// 允许跨域访问
@@ -66,7 +72,13 @@ public class ControllerInterceptor implements HandlerInterceptor {
 			log.info(String.format("%-16s%-25s%-6s%-10s%s", request.getRemoteAddr(), loginUser.getCode() + "[" + loginUser.getSessionId() + "]", "begin", "", requestUri));
 		}
 		
-		//TODO handle permission checking
+		//检查该用户是否有对该uri访问的权限
+		if (uriPermissionIdMap == null) {
+			uriPermissionIdMap = (Map<String, String>) CacheManager.getInstance().get(CacheConstants.UriPermissionIdMap);
+		}
+		if (!handlePermissionChecking(request, response, loginUser)) {
+			return false;
+		}
 		
 		return true;
 	}
@@ -137,6 +149,35 @@ public class ControllerInterceptor implements HandlerInterceptor {
 			}
 		}
 		return loginUser;
+	}
+	
+	//检查该请求的uri对应的permissionId是否在该用户的permission列表里面
+	private boolean handlePermissionChecking(HttpServletRequest request, HttpServletResponse response, LoginUser loginUser) throws Exception {
+		if (loginUser != null && loginUser.getId() != null)	{
+			String uri = request.getRequestURI();
+			uri = uri.replaceFirst(request.getContextPath(), "");
+			String requestedPermissionId = uriPermissionIdMap.get(uri);
+			if (!StringUtils.isEmpty(requestedPermissionId)) {
+				if (loginUser.getUser().getPermissionIdSet().contains(requestedPermissionId)) { //如果用户的Permission列表中有这个Permission
+					return true;
+					
+				} else {
+					log.warn(String.format("%-18s%-50s%s", request.getRemoteAddr(), request.getRequestURI(), "It is a no permission request..."));
+					if (WebUtil.isAjAxRequest(request)) {
+						response.setHeader("AJAX_REQUEST_CHECKING", MessageCode.System.AJAX_REQUEST_CHECKING_NOPERMISSION);
+						ResultBean rb = new ResultBean();
+						rb.setSuccess(false);
+						rb.setMessageCode(MessageCode.System.AJAX_REQUEST_CHECKING_NOPERMISSION);
+						rb.setMessage("用户无此功能操作权限");
+						responseResultBean(request, response, rb);
+					} else {
+						response.sendRedirect(request.getContextPath());
+					}
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	
 	private void responseResultBean(HttpServletRequest request, HttpServletResponse response, ResultBean rb) {
